b06d3474ecaf67736ad340d95f9f03aa99292c48
itscatherinelee <itscatherinehlee@gmail.com>
2020-07-29 15:36:59 -0400

    fixing my stupid stupid mistakes

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+import LandingPage from "../LandingPage/LandingPage.js";
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path ="/">
+          <LandingPage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <h> hello </h>
+        );
+    }
+}
+export default LandingPage;
