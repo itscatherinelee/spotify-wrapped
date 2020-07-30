b0633e88d33d09ead003f5ce5bd9d8430124ce55
itscatherinelee <itscatherinehlee@gmail.com>
2020-07-29 23:21:56 -0400

    add server, middleware, client folders

---
+++ client/src/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className = "container">
+        <p>time to know stuff about you</p>
+      </div>
+    );
+  }
+}
+export default LandingPage;
