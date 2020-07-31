b02bb7d0e62a8aef497a7aa6a018e496899951f4
itscatherinelee <itscatherinehlee@gmail.com>
2020-07-31 19:33:11 -0400

    adding more organization

---
+++ src/LandingView/LandingView.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class LandingView extends Component {
+  render() {
+    return(
+      <div>
+        <h2>title</h2>
+        <h2> name </h2>
+        <h2> info </h2>
+      </div>
+    );
+  }
+
+}
+export default LandingView;
